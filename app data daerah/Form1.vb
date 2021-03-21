30efc3f0b37629cd59deef18b9310a51df9efd74
Ren6848 <renaldi6848@gmail.com>
2021-03-22 01:56:52 +0700

    app data daerah

---
+++ app data daerah/Form1.vb
@@ -0,0 +1,13 @@
+﻿Imports MySql.Data.MySqlClient
+Imports System.Data.Odbc
+Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim da As New MySqlDataAdapter("select * from tbl_provinsi", KonekDb)
+        Dim dt As New DataTable
+        Dim KonekDB As New 
+        da.Fill(dt)
+        DataGridView1.DataSource = dt
+    End Sub
+
+End Class
